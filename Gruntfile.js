a8f368979fbf9f2196fa1a12494327b5a4c9a44a
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2015-02-07 14:52:14 +0100

    Gruntify theme.
    To generate theme, bootstrap 3.3.2 is needed.

---
+++ Gruntfile.js
@@ -0,0 +1,194 @@
+/*!
+ * Bootstrap's Gruntfile
+ * http://getbootstrap.com
+ * Copyright 2013-2015 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+module.exports = function (grunt) {
+  'use strict';
+
+  // Force use of Unix newlines
+  grunt.util.linefeed = '\n';
+
+  RegExp.quote = function (string) {
+    return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+  };
+
+  var fs = require('fs');
+  var path = require('path');
+  var getLessVarsData = function () {
+    var filePath = path.join(__dirname, 'less/variables.less');
+    var fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
+    var parser = new BsLessdocParser(fileContent);
+    return { sections: parser.parseFile() };
+  };
+  var generateCommonJSModule = require('./bootstrap/grunt/bs-commonjs-generator.js');
+  var configBridge = grunt.file.readJSON('./bootstrap/grunt/configBridge.json', { encoding: 'utf8' });
+
+  Object.keys(configBridge.paths).forEach(function (key) {
+    configBridge.paths[key].forEach(function (val, i, arr) {
+      arr[i] = path.join('./docs/assets', val);
+    });
+  });
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*!\n' +
+            ' * Bootstrap v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+            ' */\n',
+    jqueryCheck: configBridge.config.jqueryCheck.join('\n'),
+    jqueryVersionCheck: configBridge.config.jqueryVersionCheck.join('\n'),
+
+    // Task configuration.
+    clean: {
+      dist: 'css',
+    },
+
+    concat: {
+      bootstrap: {
+        src: [
+          'bootstrap/js/transition.bootstrap/js',
+          'bootstrap/js/alert.bootstrap/js',
+          'bootstrap/js/button.bootstrap/js',
+          'bootstrap/js/carousel.bootstrap/js',
+          'bootstrap/js/collapse.bootstrap/js',
+          'bootstrap/js/dropdown.bootstrap/js',
+          'bootstrap/js/modal.bootstrap/js',
+          'bootstrap/js/tooltip.bootstrap/js',
+          'bootstrap/js/popover.bootstrap/js',
+          'bootstrap/js/scrollspy.bootstrap/js',
+          'bootstrap/js/tab.bootstrap/js',
+          'bootstrap/js/affix.bootstrap/js'
+        ],
+        dest: 'js/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        preserveComments: 'some'
+      },
+      core: {
+        src: '<%= concat.bootstrap.dest %>',
+        dest: 'js/<%= pkg.name %>.min.js'
+      },
+    },
+
+    less: {
+      compileCore: {
+        options: {
+          strictMath: true,
+          sourceMap: true,
+          outputSourceFiles: true,
+          sourceMapURL: '<%= pkg.name %>.css.map',
+          sourceMapFilename: 'css/<%= pkg.name %>.css.map'
+        },
+        src: 'less/bootstrap.less',
+        dest: 'css/<%= pkg.name %>.css'
+      },
+      compileTheme: {
+        options: {
+          strictMath: true,
+          sourceMap: true,
+          outputSourceFiles: true,
+          sourceMapURL: '<%= pkg.name %>-theme.css.map',
+          sourceMapFilename: 'css/<%= pkg.name %>-theme.css.map'
+        },
+        src: 'less/theme.less',
+        dest: 'css/<%= pkg.name %>-theme.css'
+      }
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: configBridge.config.autoprefixerBrowsers
+      },
+      core: {
+        options: {
+          map: true
+        },
+        src: 'css/<%= pkg.name %>.css'
+      },
+      theme: {
+        options: {
+          map: true
+        },
+        src: 'css/<%= pkg.name %>-theme.css'
+      },
+    },
+
+    cssmin: {
+      options: {
+        compatibility: 'ie8',
+        keepSpecialComments: '*',
+        advanced: false
+      },
+      minifyCore: {
+        src: 'css/<%= pkg.name %>.css',
+        dest: 'css/<%= pkg.name %>.min.css'
+      },
+      minifyTheme: {
+        src: 'css/modifications.css',
+        dest: 'css/modifications.min.css'
+      },
+    },
+
+    copy: {
+      fonts: {
+        src: 'bootstrap/fonts/*',
+        dest: 'fonts/'
+      },
+    },
+
+    watch: {
+      less: {
+        files: 'less/**/*.less',
+        tasks: 'less'
+      }
+    },
+
+    exec: {
+      npmUpdate: {
+        command: 'npm update'
+      }
+    },
+
+  });
+
+
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
+  require('time-grunt')(grunt);
+
+  // Docs HTML validation task
+  grunt.registerTask('validate-html', ['jekyll:docs', 'validation']);
+
+  var runSubset = function (subset) {
+    return !process.env.TWBS_TEST || process.env.TWBS_TEST === subset;
+  };
+  var isUndefOrNonZero = function (val) {
+    return val === undefined || val !== '0';
+  };
+
+  grunt.registerTask('test-js', ['jshint:core', 'jshint:test', 'jshint:grunt', 'jscs:core', 'jscs:test', 'jscs:grunt', 'qunit']);
+
+  // JS distribution task.
+  grunt.registerTask('dist-js', ['concat', 'uglify:core', 'commonjs']);
+
+  // CSS distribution task.
+  grunt.registerTask('less-compile', ['less:compileCore', 'less:compileTheme']);
+  grunt.registerTask('dist-css', ['less-compile', 'autoprefixer:core', 'autoprefixer:theme', 'usebanner', 'csscomb:dist', 'cssmin:minifyCore', 'cssmin:minifyTheme']);
+
+  // Full distribution task.
+  grunt.registerTask('dist', ['clean:dist', 'dist-css', 'copy:fonts', 'dist-js']);
+
+  // Default task.
+  grunt.registerTask('default', ['clean:dist', 'copy:fonts', 'test']);
+
+};
