9974057110d795dd2c009f2479f8a3695ed6d906
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2015-02-21 00:09:03 +0100

    Improve start page template

--- tpl_start.php
+++ tpl_start.php
@@ -11,34 +11,27 @@ require_once(DOKU_INC.'inc/FeedParser.php');
 <div class="start-page">
     <div class="row">
         <div class="col-md-4">
-            <?php tpl_include_page('asso'); ?>
+            <div class="well">
+            <?php tpl_include_page('association:presentation'); ?>
+            </div>
         </div>
         <div class="col-md-4">
             <?php tpl_include_page('actus'); ?>
         </div>
         <div class="col-md-4">
-            <?php tpl_include_page('agenda'); ?>
+            <h2>&Eacute;v&eacute;nements du Libre</h2>
+            <?php _tpl_agenda_short(); ?>
             
         </div>
     </div>
     <div class="row">
         <div class="col-md-4">
-            <?php tpl_include_page('rencontres'); ?>
+            <?php tpl_include_page('rencontres:presentation'); ?>
         </div>
         <div class="col-md-4">
-            <?php tpl_include_page('install_parties'); ?>
+            <?php tpl_include_page('installparty:presentation'); ?>
         </div>
         <div class="col-md-4">
-            <?php tpl_include_page('ateliers'); ?>
+            <?php tpl_include_page('ateliers:presentation'); ?>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-4">
-            <?php tpl_include_page('contact'); ?>
-        </div>
-        <div class="col-md-4">
-            <?php tpl_include_page('participer'); ?>
-        </div>
-
-
-</div>
